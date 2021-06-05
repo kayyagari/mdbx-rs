4dde7a7863c427159486a92163472f1dbafd03ab
Artem Vorotnikov <artem@vorotnikov.me>
2021-06-05 04:20:50 +0300

    Statically link mdbx

--- mdbx-sys/build.rs
+++ mdbx-sys/build.rs
@@ -9,9 +9,10 @@ fn main() {
     generate::generate();
 
     let mut dst = Config::new("libmdbx")
+        .define("MDBX_INSTALL_STATIC", "ON")
         .define("MDBX_TXN_CHECKOWNER", "OFF")
         .build();
     dst.push("lib");
     println!("cargo:rustc-link-search=native={}", dst.display());
-    println!("cargo:rustc-link-lib=mdbx");
+    println!("cargo:rustc-link-lib=static=mdbx-static");
 }
